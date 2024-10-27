009a81a7ad79fe7c53e212b1a496ef612b4bd188
DharmilTrivedi <dharmiltrivedi5@gmail.com>
2024-10-27 11:09:37 +0000

    Add Arithmetic, Boolean, Falsy Values

--- 01. Beginners/app.js
+++ 01. Beginners/app.js
@@ -7,3 +7,34 @@ let name = "Dharmil";
 var whatYouWannaBecomeInYourLife = "Programmer";
 const gender = "Male";
 const twitterHandle = "Dharmil20";
+
+//Arithmetic
+const firstFavNum = 2
+const secondFavNum = 10
+
+console.log(firstFavNum + secondFavNum)
+console.log(firstFavNum - secondFavNum)
+console.log(firstFavNum * secondFavNum)
+console.log(firstFavNum / secondFavNum)
+console.log(firstFavNum % secondFavNum)
+console.log(firstFavNum ** secondFavNum)
+
+//Boolean
+const isJsProgrammingLanguage = true;
+const isJsHard = false;
+const favNum = 20;
+console.log(isJsProgrammingLanguage)
+console.log(isJsHard)
+console.log(favNum + undefined)
+
+/*Falsy values
+ -> false
+ -> null
+ -> undefined
+ -> 0
+ -> -0
+ -> NaN
+ -> '', "", ``, (empty quotes)
+*/
+
+//Comparison 
