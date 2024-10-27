f7333a2dbfd67de938c0e858f3a74b333b996dd6
DharmilTrivedi <dharmiltrivedi5@gmail.com>
2024-10-27 10:54:10 +0000

    Add Basics

---
+++ 01. Beginners/app.js
@@ -0,0 +1,9 @@
+console.table({name: "Dharmil", age: 20});
+console.warn("This is a warning");
+console.error("This is an Error");
+
+// Variables
+let name = "Dharmil";
+var whatYouWannaBecomeInYourLife = "Programmer";
+const gender = "Male";
+const twitterHandle = "Dharmil20";
