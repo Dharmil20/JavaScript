b50880ebad5269e2db08b74e2f1e4ad0a39fe0d8
DharmilTrivedi <dharmiltrivedi5@gmail.com>
2024-10-27 12:10:58 +0000

    Add relational, string mamipulation, type conversion

--- 01. Beginners/app.js
+++ 01. Beginners/app.js
@@ -1,4 +1,4 @@
-console.table({name: "Dharmil", age: 20});
+console.table({ name: "Dharmil", age: 20 });
 console.warn("This is a warning");
 console.error("This is an Error");
 
@@ -9,23 +9,23 @@ const gender = "Male";
 const twitterHandle = "Dharmil20";
 
 //Arithmetic
-const firstFavNum = 2
-const secondFavNum = 10
+const firstFavNum = 2;
+const secondFavNum = 10;
 
-console.log(firstFavNum + secondFavNum)
-console.log(firstFavNum - secondFavNum)
-console.log(firstFavNum * secondFavNum)
-console.log(firstFavNum / secondFavNum)
-console.log(firstFavNum % secondFavNum)
-console.log(firstFavNum ** secondFavNum)
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum ** secondFavNum);
 
 //Boolean
 const isJsProgrammingLanguage = true;
 const isJsHard = false;
 const favNum = 20;
-console.log(isJsProgrammingLanguage)
-console.log(isJsHard)
-console.log(favNum + undefined)
+console.log(isJsProgrammingLanguage);
+console.log(isJsHard);
+console.log(favNum + undefined);
 
 /*Falsy values
  -> false
@@ -37,4 +37,44 @@ console.log(favNum + undefined)
  -> '', "", ``, (empty quotes)
 */
 
-//Comparison 
+//Comparison
+console.log(firstFavNum > secondFavNum);
+console.log(firstFavNum < secondFavNum);
+console.log(firstFavNum >= secondFavNum);
+console.log(firstFavNum <= secondFavNum);
+
+console.log(firstFavNum === secondFavNum); //Strict Equality operator. (checks type + value)
+console.log(firstFavNum !== secondFavNum); //Strict non-equality operator. (checks type + value)
+console.log(firstFavNum == secondFavNum); //loose equality operator. (checks value)
+console.log(firstFavNum != secondFavNum); //loose non-equality operator. (checks value)
+
+// String Manipulations
+const firstName = "Dharmil";
+const lastName = "Trivedi";
+const fullName = firstName + " " + lastName;
+const upperCase = fullName.toUpperCase();
+const message = `My favorite Actor Is ${fullName} & say something about you, Mr. ${upperCase}`;
+
+console.log(message + " his best show is Silicon Valley");
+console.log(firstName.split('').join("-"))
+
+// Type Conversion
+let amount = 100;
+let money = "100";
+let floatValue = "99.5";
+
+// -> Convert String to number
+// money = parseInt(money);
+// money = +money;
+// money = Number(money);
+// console.log(typeof money)
+
+// -> Convert number to string
+// amount = amount.toString();
+// amount = String(amount);
+// console.log(typeof amount);
+
+// -> Convert string to float
+floatValue = parseFloat(floatValue);
+console.log(floatValue);
+console.log(typeof floatValue);
