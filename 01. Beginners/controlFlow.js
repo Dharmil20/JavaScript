400b85a5f834a080115e0e6db67d9b9cdf1a8c1c
DharmilTrivedi <dharmiltrivedi5@gmail.com>
2024-10-28 06:56:23 +0000

    Add controlFlow

---
+++ 01. Beginners/controlFlow.js
@@ -0,0 +1,21 @@
+// if conditions
+// let password;
+
+// if (password === 8) {
+//   console.log("welcome");
+// } else if (password <= 8) {
+//   console.log("Password is too short");
+// } else if (password >= 8) {
+//   console.log("firstToo Long Password & Password should be 8 characters");
+// } else {
+//   console.log("Please provide a password");
+// }
+
+// Array
+const favSingers = ["a", "b", "c"];
+console.log(favSingers[0]);
+const favNumbers = [1, 2, 3, 4];
+const mixedArr = ["abc", [1, 2, 3], 1, true];
+for (let i = 0; i < mixedArr.length; i++) {
+  console.log(mixedArr[i])
+}
